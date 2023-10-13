2b1fe451c4d812c97dda2c68d1e6e6f27cd2ecfd
fathpurn <talitakamilia345@gmail.com>
2023-10-13 11:15:28 +0700

    setup project

---
+++ README.md
@@ -0,0 +1,1 @@
+# PKMRuntah-Mobile
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const { Component } = require('react');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.js", "./components/**/*.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
